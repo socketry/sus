45afaa12044de51f439d117e36b9a5bf1ead3035
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-01-07 00:53:32 +1300

    Expand testing to include Ruby 2.7 and Ruby 3.1.

--- test/sus/identity.rb
+++ test/sus/identity.rb
@@ -1,6 +1,8 @@
 describe Sus::Identity do
 	with 'basic identifier' do
-		def identifier = subject.new("file.rb", "file", 123)
+		def identifier
+			subject.new("file.rb", "file", 123)
+		end
 		
 		it "can generate unique identifier" do
 			expect(identifier.key).to be == "file.rb:123"
@@ -8,8 +10,13 @@ describe Sus::Identity do
 	end
 	
 	with 'nested unique identifiers' do
-		def parent = subject.new("file1.rb", "file1", 10)
-		def identifier = subject.new("file2.rb", "file2", 20, parent)
+		def parent
+			subject.new("file1.rb", "file1", 10)
+		end
+		
+		def identifier
+			subject.new("file2.rb", "file2", 20, parent)
+		end
 		
 		it "can generate unique identifier" do
 			expect(identifier.key).to be == "file1.rb:20"
@@ -17,8 +24,13 @@ describe Sus::Identity do
 	end
 	
 	with 'nested non-unique identifiers' do
-		def parent = subject.new("file1.rb", "file1", 10, unique: false)
-		def identifier = subject.new("file2.rb", "file2", 20, parent, unique: false)
+		def parent
+			subject.new("file1.rb", "file1", 10, unique: false)
+		end
+		
+		def identifier
+			subject.new("file2.rb", "file2", 20, parent, unique: false)
+		end
 		
 		it "can generate unique identifier" do
 			expect(identifier.key).to be == "file1.rb:10:20"
@@ -26,8 +38,13 @@ describe Sus::Identity do
 	end
 	
 	with 'nested non-unique named identifiers' do
-		def parent = subject.new("file1.rb", "file1", 10, unique: "one")
-		def identifier = subject.new("file2.rb", "file2", 20, parent, unique: false)
+		def parent
+			subject.new("file1.rb", "file1", 10, unique: "one")
+		end
+		
+		def identifier
+			subject.new("file2.rb", "file2", 20, parent, unique: false)
+		end
 		
 		it "can generate unique identifier" do
 			expect(identifier.key).to be == "file1.rb:one:20"
