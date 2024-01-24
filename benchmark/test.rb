fe0da3d19b2f43e314a273464cbd2cfbc3fcc177
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-01-25 00:54:48 +1300

    Add benchmark.

---
+++ benchmark/test.rb
@@ -0,0 +1,17 @@
+ITERATIONS = ENV.fetch("ITERATIONS", 1000).to_i
+TYPE = ENV.fetch("TYPE", "pos")
+
+describe "sus" do
+  ITERATIONS.times do |n|
+    case TYPE
+    when "pos" then
+      it "pos #{n}" do
+        expect(1).to be == 1
+      end
+    when "neg" then
+      it "neg #{n}" do
+        expect(1).to be == 2
+      end
+    end
+  end
+end
